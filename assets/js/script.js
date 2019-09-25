d33d78cfb04f722f368efbbf81f377099fe4f416
Amjad Shaaban <theamjad@outlook.com>
2019-09-25 16:31:10 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBQsf3G2IkFi4MeOtDEyMv7mEHAGz37MY4",
+  authDomain: "rock-paper-scissors-with-chat.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-with-chat.firebaseio.com",
+  projectId: "rock-paper-scissors-with-chat",
+  storageBucket: "",
+  messagingSenderId: "960996149541",
+  appId: "1:960996149541:web:ecfc2e8970aa293b6bf2cc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

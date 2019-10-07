81f8481ace718d1b8ab8af09fb9351de8eac8723
Amjad Shaaban <theamjad@outlook.com>
2019-10-07 18:40:20 -0400

    NO CHAT YET!

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,128 +1,55 @@
-let p1Choice = "r";
-let p2Choice = "s";
+$(document).ready(function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyBQsf3G2IkFi4MeOtDEyMv7mEHAGz37MY4",
+    authDomain: "rock-paper-scissors-with-chat.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-with-chat.firebaseio.com",
+    projectId: "rock-paper-scissors-with-chat",
+    storageBucket: "",
+    messagingSenderId: "960996149541",
+    appId: "1:960996149541:web:ecfc2e8970aa293b6bf2cc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
-const timeline = anime.timeline();
-const timeline2 = anime.timeline();
+  var p1Choice = "r";
+  var p2Choice = "s";
 
-// timeline
-//   .add({
-//     targets: ".p1",
-//     translateY: [0, 200],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p1",
-//     translateY: [200, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p1",
-//     translateY: [0, 200],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p1",
-//     translateY: [200, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p1",
-//     translateY: [0, 200],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p1",
-//     translateY: [200, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   });
-// timeline2
-//   .add({
-//     targets: ".p2",
-//     translateY: [0, 500],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p2",
-//     translateY: [500, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p2",
-//     translateY: [0, 500],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p2",
-//     translateY: [500, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p2",
-//     translateY: [0, 500],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   })
-//   .add({
-//     targets: ".p2",
-//     translateY: [500, 0],
-//     direction: "alternate",
-//     easing: "spring(1, 80, 10, 0)",
-//     duration: 100
-//   });
+  function result() {
+    $(".p1").css("border", "0px solid black");
+    $(".p2").css("border", "0px solid black");
 
-function result() {
-  $(".p1").css("border", "0px solid black");
-  $(".p2").css("border", "0px solid black");
+    anime({
+      targets: [".p1", ".p2"],
+      translateY: [{ value: 200, duration: 300 }],
+      direction: "alternate",
+      loop: 5,
+      easing: "linear"
+    });
+  }
 
-  anime({
-    targets: [".p1", ".p2"],
-    translateY: [{ value: 200, duration: 300 }],
-    direction: "alternate",
-    loop: 5,
-    easing: "linear"
+  function show() {
+    $(".l").attr("src", "./assets/img/scis-l.png");
+    $(".p1").css({
+      border: "5px solid black",
+      background: "lime"
+    });
+    $(".r").attr("src", "./assets/img/paper-r.png");
+    $(".p2").css({
+      border: "5px solid black",
+      background: "red"
+    });
+  }
+  database.ref().on("value", function(snab) {
+    var data = snab.val();
+    if (data.players === undefined) {
+      console.log("off");
+    } else if (data.players.ready === null) {
+      console.log("no db");
+    } else if (data.players.ready) {
+      console.log("animate");
+      result();
+      setTimeout(show, 2000);
+    }
   });
-}
-
-function show() {
-  $(".l").attr("src", "./assets/img/scis-l.png");
-  $(".p1").css({
-    border: "5px solid black",
-    background: "lime"
-  });
-  $(".r").attr("src", "./assets/img/paper-r.png");
-  $(".p2").css({
-    border: "5px solid black",
-    background: "red"
-  });
-}
-result();
-setTimeout(show, 2000);
-// show();
-//   $(animation).once();
-// }
-// $(document)
-//   .once(result())
-//   .then(() => {
-//
-//   });
+});
